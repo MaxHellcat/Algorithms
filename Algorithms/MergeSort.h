747e70aab9f94939a3bd1fc0304302a7f5f2b0dd
Max Reshetey <maxim.reshetey@gmail.com>
2017-11-14 14:00:36 +0300

    Draft merge sort for linked lists (not working yet).

--- Algorithms/MergeSort.h
+++ Algorithms/MergeSort.h
@@ -10,8 +10,145 @@
 #define MergeSort_h
 
 #include "Array.h"
+#include "DoublyLinkedList.h"
 
 // TODO: Implement merge sort for linked list!
+
+using Node = DoublyLinkedList<int>::Node;
+
+void merge_DLL(DoublyLinkedList<int> &list, Node **leftNode, Node **midNode, Node **rightNode);
+
+struct Info
+{
+	Info(Node *left, Node *mid, Node *right): leftNode(left), midNode(mid), rightNode(right) {}
+
+	Node *leftNode, *midNode, *rightNode;
+};
+
+// TODO: Draft version, not working properly
+Info mergeSort_DLL(DoublyLinkedList<int> &list, Node *leftNode, Node *rightNode)
+{
+	std::cout << "(" << leftNode->key << ", " << rightNode->key << ")" << std::endl;
+	
+	Node *outerMidNode = leftNode;
+
+	if (leftNode != rightNode)
+	{
+		// Divide (locate middle node between left and right nodes)
+		auto node = leftNode;
+
+		int count = 1;
+		while (node != rightNode)
+		{
+			node = node->next;
+			count++;
+		}
+
+		const int kNumberOfNodes = count;
+
+		auto midNode = leftNode;
+		count = 1;
+		while (count < kNumberOfNodes/2)
+		{
+			midNode = midNode->next;
+			count++;
+		}
+		
+		outerMidNode = midNode;
+
+		// Conquer
+		mergeSort_DLL(list, leftNode, midNode);
+		Info info = mergeSort_DLL(list, midNode->next, rightNode);
+		
+		leftNode = info.leftNode;
+		midNode = info.midNode;
+		rightNode = info.rightNode;
+
+		// Combine
+		merge_DLL(list, &leftNode, &midNode, &rightNode);
+	}
+	
+	return Info(leftNode, outerMidNode, rightNode);
+}
+
+void merge_DLL(DoublyLinkedList<int> &list, Node **firstNode, Node **midNode, Node **lastNode)
+{
+	const auto kFirstNodePrev = (*firstNode)->prev;
+	const auto kLastNodeNext = (*lastNode)->next;
+
+	auto leftNode = *firstNode;
+	auto rightNode = (*midNode)->next;
+
+//	auto node = leftNode;
+//	int count = 1;
+//	while (node != *lastNode)
+//	{
+//		node = node->next;
+//		count++;
+//	}
+//
+//	const int kNumberOfNodes = count;
+//	count = 1;
+
+	while (true)
+	{
+		if (leftNode->key <= rightNode->key)
+		{
+			leftNode = leftNode->next;
+		}
+		else
+		{
+			const auto rightNodeNext = rightNode->next;
+
+//			7, 8, 5, 6
+//			5, 7, 8, 6
+//			6, 5, 7, 8
+
+			*midNode = rightNode;
+
+			// Cut mid from current place
+			if (rightNode->prev) {
+				rightNode->prev->next = rightNode->next;
+			}
+			if (rightNode->next) {
+				rightNode->next->prev = rightNode->prev;
+			}
+
+			rightNode->prev = nullptr;
+			rightNode->next = nullptr;
+			list.insert(rightNode, leftNode);
+
+			rightNode = rightNodeNext; // jumps over lastNode finally
+			
+			if (rightNode == kLastNodeNext || leftNode == *midNode)
+			{
+				std::cout << "Break cos condition!" << std::endl;
+				break;
+			}
+
+		}
+
+//		count++;
+	}
+
+	if (kFirstNodePrev) {
+		*firstNode = kFirstNodePrev->next;
+	}
+	else {
+		*firstNode = list.head();
+	}
+	if (kLastNodeNext) {
+		*lastNode = kLastNodeNext->prev;
+	}
+	else
+	{
+		auto node = list.head();
+		while (node->next) { node = node->next; }
+		*lastNode = node;
+	}
+
+	std::cout << list.description() << std::endl;
+}
 
 void merge(Array<int> &arr, int leftIndex, int midIndex, int rightIndex);
 
@@ -107,6 +244,29 @@ void test_mergeSort()
 
 		std::cout << std::endl;
 	}
+}
+
+void test_mergeSort_DLL()
+{
+	initTestEnvironment();
+
+	DoublyLinkedList<int> list;
+
+	for (int i = 8; i > 0; i--)
+	{
+		list.insert(new DoublyLinkedList<int>::Node(i), nullptr);
+	}
+
+	std::cout << "Original list: " << std::endl;
+	std::cout << list.description() << std::endl;
+
+	auto node = list.head();
+	while (node->next) { node = node->next; }
+
+	mergeSort_DLL(list, list.head(), node);
+
+	std::cout << "Sorted list: " << std::endl;
+	std::cout << list.description() << std::endl;
 }
 
 #endif /* MergeSort_h */
